835f24032306bfcebd7b2a4dfc268a512d486f0e
you@example.com <you@example.com>
2021-08-30 16:52:17 +0530

    msg

--- forex/admin.py
+++ forex/admin.py
@@ -1,12 +1,13 @@
 from .models import ExpenseTransaction
 from ra.admin.admin import ra_admin_site, EntityAdmin, TransactionAdmin, TransactionItemAdmin
 from django import forms
+from import_export.admin import ImportExportModelAdmin
 
-class GeneralTransactionAdmin(TransactionAdmin):
+class GeneralTransactionAdmin(ImportExportModelAdmin):
     model = ExpenseTransaction
-    list_display = [ 'type', 'customer', 'employee', 'value','creation_date','status','percent_per_month','payout','amount_left', 'account_number', 'account_type']
+    list_display = ['customer', 'employee', 'value','type','creation_date','status','percent_per_month','payout','amount_left', 'account_number', 'account_type']
     fields = [ 'type', 'customer', 'employee', 'value','doc_date','status','percent_per_month','amount_left', 'notes', 'account_type', 'account_number', 'email']
-    list_display_links = ('type', 'customer', 'employee')
+    list_display_links = ('customer', 'employee')
     search_fields = ('type', 'customer', 'notes')
 
 ra_admin_site.register(ExpenseTransaction, GeneralTransactionAdmin)
